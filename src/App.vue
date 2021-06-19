00b1823524b5786ab1cebc9968cfd94873be560a
NFSS10 <nuno_nfss96@hotmail.com>
2021-06-19 20:50:27 +0100

    App using full height

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,15 @@
 </template>
 
 <style>
+html > body {
+    height: 100vh;
+    margin: 0px 0px 0px 0px;
+}
+
 #app {
+    box-sizing: border-box;
+    height: 100%;
+    padding: 8px 8px 8px 8px;
     font-family: "Avenir", Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
